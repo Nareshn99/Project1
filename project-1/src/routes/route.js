fe779f996759f92f056187aa47ede401f927198c
Harikesh9690 <harikesmtr02018@gmail.com>
2022-09-05 16:54:38 +0530

    project-1

---
+++ project-1/src/routes/route.js
@@ -0,0 +1,9 @@
+const { Router } = require('express')
+const express = require('express')
+const router = express.Router()
+
+router.get("/test-me",function(req,res){
+    res.send("maari api testing")
+})
+
+module.exports = router
